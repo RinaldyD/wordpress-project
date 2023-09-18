38dd92b6156a25809cddcea400921562f85dff7b
RinaldyD <darmawan.rinaldy@gmail.com>
2023-09-18 21:26:56 +0700

    create deployment and service

--- main.tf
+++ main.tf
@@ -227,3 +227,98 @@ resource "null_resource" "configure_kubectl" {
     google_container_cluster.gke_cluster1
   ]
 }
+
+//WordPress Deployment
+resource "kubernetes_deployment" "wp-dep" {
+  metadata {
+    name   = "wp-dep"
+    labels = {
+      env     = "Production"
+    }
+  }
+
+  spec {
+    replicas = 3
+    selector {
+      match_labels = {
+        pod     = "wp"
+        env     = "Production"
+      }
+    }
+
+    template {
+      metadata {
+        labels = {
+          pod     = "wp"
+          env     = "Production"
+        }
+      }
+
+      spec {
+        container {
+          image = "wordpress:latest"
+          name  = "wp-container"
+
+          env {
+            name  = "WORDPRESS_DB_HOST"
+            value = "${google_sql_database_instance.sqldb_Instance.ip_address.0.ip_address}"
+          }
+          env {
+            name  = "WORDPRESS_DB_USER"
+            value = var.db_user
+          }
+          env {
+            name  = "WORDPRESS_DB_PASSWORD"
+            value = var.db_user_pass
+          }
+          env{
+            name  = "WORDPRESS_DB_NAME"
+            value = var.database
+          }
+          env{
+            name  = "WORDPRESS_TABLE_PREFIX"
+            value = "wp_"
+          }
+
+          port {
+            container_port = 80
+          }
+        }
+      }
+    }
+  }
+
+  depends_on = [
+    null_resource.set_gcloud_project,
+    google_container_cluster.gke_cluster1,
+    google_container_node_pool.nodepool1,
+    null_resource.configure_kubectl,
+  ]
+}
+
+//Creating LoadBalancer Service
+resource "kubernetes_service" "wpService" {
+    metadata {
+        name   = "wp-svc"
+        labels = {
+        env     = "Production" 
+        }
+    }  
+
+    spec {
+        type     = "LoadBalancer"
+        selector = {
+        pod = "${kubernetes_deployment.wp-dep.spec.0.selector.0.match_labels.pod}"
+        }
+
+        port {
+        name = "wp-port"
+        port = 80
+        target_port = 80
+        }
+    }
+
+    depends_on = [
+        kubernetes_deployment.wp-dep,
+    ]
+}
